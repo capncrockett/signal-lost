ad18431d755191daaec6f380d692f05d6b9b63b6
Capn Crockett <crockettdevlabs@gmail.com>
2025-04-08 16:35:07 -0700

    [WIP] Remove tests-archive directory

--- tests-archive/tests/mocks/AudioManagerMock.ts
+++
@@ -1,9 +0,0 @@
-// Create a mock for the AudioManager
-export const mockAudioManager = {
-  getInstance: jest.fn().mockReturnValue({
-    setMasterVolume: jest.fn(),
-    getMasterVolume: jest.fn().mockReturnValue(0.1),
-    addVolumeChangeListener: jest.fn(),
-    removeVolumeChangeListener: jest.fn(),
-  }),
-};
