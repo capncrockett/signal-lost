a30f9b0a3c3118ee05dd4f694436532848292d5b
CSL-Beta <crockettsoundlabs@gmail.com>
2025-04-11 09:37:05 -0700

    [Beta] Fix unsafe assignment in radioStore.ts

--- src/stores/radioStore.ts
+++ src/stores/radioStore.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { create } from 'zustand';
 
 interface RadioState {
