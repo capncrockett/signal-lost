e00cbed93bcee92a61e9197dae84998e79b67dce
Capn Crockett <crockettdevlabs@gmail.com>
2025-04-14 20:44:55 -0700

    Fix batch scripts: update timestamp generation and fix path quoting

--- godot_project/run_tests.bat
+++ godot_project/run_tests.bat
@@ -27,7 +27,7 @@ if not exist "%DIR%addons\gut" (
 )
 
 REM Run the test runner script
-"%GODOT_EXECUTABLE%" --path "%DIR%" --script tests/test_runner.gd
+"%GODOT_EXECUTABLE%" --path "%DIR%" --script "tests/test_runner.gd"
 
 REM Get the exit code
 set EXIT_CODE=%ERRORLEVEL%
